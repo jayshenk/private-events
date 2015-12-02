480e442c3d7c7a7ba72817f59a7cef8700dd9645
Jay Shenk <jay.shenk@gmail.com>
2015-12-01 20:26:03 -0500

    Add Event model and associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151128193855) do
+ActiveRecord::Schema.define(version: 20151201024750) do
+
+  create_table "events", force: :cascade do |t|
+    t.text     "description"
+    t.datetime "event_date"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "creator_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"

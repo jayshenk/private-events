95710ed7d09f5caa4e0373fcd45d5c0d00576d9b
Jay Shenk <jay.shenk@gmail.com>
2015-12-05 21:04:16 -0500

    Add Events controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root               'events#index'
   get    'login'  => 'sessions#new'
   post   'login'  => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:show, :new, :create]
+  resources :events, only: [:index, :show, :new, :create]
 end

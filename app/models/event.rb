d7ff4a8df226058fcc85245e511029959f7ae58f
Jay Shenk <jay.shenk@gmail.com>
2015-12-06 19:39:16 -0500

    Add logic for past and upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ActiveRecord::Base
   belongs_to :creator, class_name: "User"
   has_many :event_attendings, foreign_key: :attended_event_id
   has_many :attendees, through: :event_attendings
+
+  def Event.past
+    Event.where("event_date < ?", Time.now)
+  end
+
+  def Event.upcoming
+    Event.where("event_date > ?", Time.now)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ActiveRecord::Base
   has_many :created_events, foreign_key: :creator_id, class_name: "Event"
   has_many :event_attendings, foreign_key: :attendee_id
   has_many :attended_events, through: :event_attendings
+
+  def upcoming_events
+    self.attended_events.upcoming
+  end
+
+  def previous_events
+    self.attended_events.past
+  end
 end
